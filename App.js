0a9ee019e033108cd0e5ac0530aa92c3976a25c3
Boluwatife Omosowon <boluomosowon@gmail.com>
2021-11-16 19:51:40 +0100

    added post and get functions

--- App.js
+++ App.js
@@ -1,15 +1,64 @@
 const express = require('express');
 const app = express();
+const Joi = require('joi');
 
 const { quotes } = require('./data/quotes');
 const { getRandomElement } = require("./utils/getRandomElement");
 
+app.use(express.json());
 
 
 app.get("/api/quotes/random", (req, res) => {
   const quote = getRandomElement(quotes);
-  res.send(quote );
+  res.send(quote);
 });
+
+
+app.get("/api/quotes", (req, res) => {
+  res.send(quotes);
+});
+
+app.get("/api/quotes/:author", (req, res) => {
+
+  const result = quotes.filter((quote) => {
+    return quote["author"] === req.params['author']
+  });
+
+  if (result == 0) {
+    res.send("Oops, looks like we don't have that author in our database");
+  } else {
+    res.send(result);
+  }
+});
+
+
+app.post('/api/quotes', (req, res) => {
+
+  const { error } = validateQuote(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+
+  const quote = {
+    quote: req.body.quote,
+    author: req.body.author
+  };
+
+  if (quote.author.length == 0) {
+    quote.author = "Unknown";
+  }
+
+  quotes.push(quote);
+  res.send(quote);
+});
+
+
+function validateQuote(quote) {
+  const schema = Joi.object({
+    quote: Joi.string().required(),
+    author: Joi.string()
+  });
+  return schema.validate(quote);
+}
 
 
 const port = process.env.PORT || 3000;
--- data/quotes.js
+++ data/quotes.js
@@ -1,54 +1,54 @@
 const quotes = [
     {
-        quotes: "All our dreams can come true, if we have the courage to pursue them",
+        quote: "All our dreams can come true, if we have the courage to pursue them",
         author: "Walt Disney",
     },
     {
-        quotes: "The secret of getting ahead is getting started.",
+        quote: "The secret of getting ahead is getting started.",
         author: "Mark Twain",
     },
     {
-        quotes: "Don’t limit yourself. Many people limit themselves to what they think they can do. You can go as far as your mind lets you. What you believe, remember, you can achieve.",
+        quote: "Don’t limit yourself. Many people limit themselves to what they think they can do. You can go as far as your mind lets you. What you believe, remember, you can achieve.",
         author: "Mary Kay Ash",
     },
     {
-        quotes: "The best time to plant a tree was 20 years ago. The second best time is now.",
+        quote: "The best time to plant a tree was 20 years ago. The second best time is now.",
         author: "Chinese Proverb",
     },
     {
-        quotes: "The same boiling water that softens the potato hardens the egg. It’s what you’re made of. Not the circumstances.",
+        quote: "The same boiling water that softens the potato hardens the egg. It’s what you’re made of. Not the circumstances.",
         author: "Unknown",
     },
     {
-        quotes: "The hard days are what make you stronger.",
+        quote: "The hard days are what make you stronger.",
         author: "Aly Raisman",
     },
     {
-        quotes: "In a gentle way, you can shake the world.",
+        quote: "In a gentle way, you can shake the world.",
         author: "Mahatma Gandhi",
     },
     {
-        quotes: "Turn your wounds into wisdom",
+        quote: "Turn your wounds into wisdom",
         author: "Oprah",
     },
     {
-        quotes: "If you don't like someone's story, write your own.",
+        quote: "If you don't like someone's story, write your own.",
         author: "Chinua Achebe",
     },
     {
-        quotes: "One of the truest tests of integrity is its blunt refusal to be compromised.",
+        quote: "One of the truest tests of integrity is its blunt refusal to be compromised.",
         author: "Chinua Achebe",
     },
     {
-        quotes: "Just try new things. Don't be afraid. Step out of your comfort zones and soar, all right?",
+        quote: "Just try new things. Don't be afraid. Step out of your comfort zones and soar, all right?",
         author: "Michelle Obama",
     },
     {
-        quotes: "We become what we think about",
+        quote: "We become what we think about",
         author: "Earl Nightingale",
     },
     {
-        quotes: "Optimism is the one quality more associated with success and happiness than any other.",
+        quote: "Optimism is the one quality more associated with success and happiness than any other.",
         author: "Brian Tracy",
     },
 ]
